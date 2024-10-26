23430185f9d10601466481dd92e833ad19d48d97
PieterNatanael <pieternatanael@icloud.com>
2024-10-26 14:48:36 +0800

    Initial Commit

---
+++ Baby Timely/Baby_TimelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baby_TimelyApp.swift
+//  Baby Timely
+//
+//  Created by Pieter Yoshua Natanael on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Baby_TimelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baby TimelyTests/Baby_TimelyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Baby_TimelyTests.swift
+//  Baby TimelyTests
+//
+//  Created by Pieter Yoshua Natanael on 26/10/24.
+//
+
+import Testing
+@testable import Baby_Timely
+
+struct Baby_TimelyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
